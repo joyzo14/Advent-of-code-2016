b46f19390e897dd3e52e542c3952f4c434ab687a
joyzo14 <hostak33@gmail.com>
2017-01-01 19:36:33 +0100

    Add files via upload

--- advent_exam_1_2.py
+++ advent_exam_1_2.py
@@ -1,22 +1,33 @@
-# Hello World program in Python
+#initial coordinates
 x = 0
 y = 0
+
+#coordinates of movements
 x2 = []
 y2 = []
+
+#initial side
 side = "N"
+
+#prepared directions and distance from file
 direction = ["R","R","L","R","L","R","R","R","L","R","L","R","L","L","L","R","R","R","L","L","R","L","R","R","L","R","L","L","R","R","R","L","L","R","L","L","R","L","R","L","R","L","L","R","R","L","R","L","R","R","R","L","L","L","R","R","R","L","R","L","L","R","R","L","L","R","L","R","R","R","R","R","L","R","R","L","R","R","R","L","R","R","L","R","L","L","L","R","R","L","R","R","L","L","R","L","L","R","L","L","L","R","R","R","L","L","L","R","L","R","R","L","L","L","R","R","L","R","L","R","R","L","R","L","R","L","R","R","L","R","R","R","L","L","R","R","L","L","R","L","L","R","R","R","L","L","L","L","R","L","L","R","R","R","R","L","R","R","L","L","L","L","L","R","L","L","L","L","R"]
 distance = [4,4,1,3,5,2,5,1,4,3,5,2,3,4,3,1,5,1,3,1,3,1,2,2,2,5,3,4,4,4,2,4,1,5,1,4,4,1,1,2,5,2,3,2,1,194,2,4,49,1,3,5,4,1,4,2,1,5,3,5,4,4,4,2,3,78,5,4,191,4,3,1,2,1,3,1,3,4,2,2,1,4,5,2,2,4,2,1,2,3,5,2,3,3,3,1,1,5,4,4,2,5,1,4,3,5,4,5,4,5,4,3,2,5,4,3,3,1,5,5,1,3,2,5,5,3,1,4,5,4,2,3,4,5,3,4,5,5,4,4,4,1,5,3,1,4,3,4,1,5,1,2,2,4,4,5,4,1,1,1,3,5,2,4,3,5,4,1,3,3]
+
+#check if there is intersection and in positive case print occurences 
 def check():
     for a, b in enumerate(x2):
         if a + 1 == len(x2):
             continue
         elif ((x == x2[a]) and (y == y2[a])):
-            print([x,y])
+            #print every occurences of intersection
+            print("Shortest path to the destination is "+str(abs(0-x)+abs(0-y)))
     
+#add coordinates to list    
 def add():
     x2.append(x)
     y2.append(y)
-    
+ 
+ #browse every item in direction list and after every iterate get a new coordinates and new actual side
 for number,item in enumerate(direction):
      if side == "N":
          if direction[number] == "R":
@@ -70,6 +81,3 @@ for number,item in enumerate(direction):
                  x += 1
                  add()
                  check()
-
-
-print([x,y])
